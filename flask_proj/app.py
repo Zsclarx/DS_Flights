9f27b0bf1e14810151de23717d7b4761d9c66045
Zsclarx <prakhar2704@gmail.com>
2024-07-03 20:15:58 +0530

    Third

---
+++ flask_proj/app.py
@@ -0,0 +1,46 @@
+from flask import (Flask,url_for,render_template)
+from forms import InputForm
+import joblib
+import pandas as pd
+
+
+model = joblib.load(r"C:\Users\Prakhar\OneDrive\Desktop\Prakhar\DS_Flights\flask_proj\model.joblib")
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "secret_key"
+
+@app.route("/")
+
+@app.route("/home")
+def home():
+    return render_template("home.html",title = "Home")
+
+
+@app.route("/predict",methods=["GET","POST"])
+def predict():
+    form = InputForm()
+    if form.validate_on_submit():
+        xnew = pd.DataFrame(dict(
+            airline  = [form.airline.data],
+            date_of_journey  = [form.dateofjoin.data.strftime("%Y-%m-%d")],
+            source  = [form.source.data],
+            destination  = [form.destination.data],
+            dep_time  = [form.dept_time.data.strftime("%H:%M:%S")],
+            arrival_time  = [form.arrival_time.data.strftime("%H:%M:%S")],
+            duration  = [form.duration.data],
+            total_stops  = [form.total_stops.data],
+            additional_info  = [form.additional_info.data]
+        )
+        )
+        prediction = model.predict(xnew)[0]
+        message = f"The predicted price is INR {prediction} Rs."
+
+    else:
+        message = "Please Provide valid input details."
+    return render_template("predict.html",title = "Predict",form = form,message = message)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ flask_proj/forms.py
@@ -0,0 +1,73 @@
+from flask_wtf import FlaskForm
+import pandas as pd
+
+from wtforms import (
+    SelectField,
+    DateField,
+    TimeField,
+    IntegerField,
+    SubmitField,
+)
+
+from wtforms.validators import(
+    DataRequired,
+) 
+
+
+train = pd.read_csv(r'C:\Users\Prakhar\OneDrive\Desktop\Prakhar\DS_Flights\data\train (1).csv')
+val = pd.read_csv(r'C:\Users\Prakhar\OneDrive\Desktop\Prakhar\DS_Flights\data\val.csv')
+x_data = pd.concat([train, val],axis=0).drop(columns='price')
+
+class InputForm(FlaskForm):
+    airline = SelectField(
+        label = 'airline',
+        choices= x_data.airline.unique().tolist(),
+        validators=[DataRequired()]
+    )
+    dateofjoin = DateField(
+        label = 'date of journey',
+        validators=[DataRequired()]
+    )
+    source = SelectField(
+        label = 'Source',
+        choices = x_data.source.unique().tolist(),
+        validators=[DataRequired()]
+    )
+
+    destination = SelectField(
+        label = 'Destination',
+        choices = x_data.destination.unique().tolist(),
+        validators=[DataRequired()]
+    )
+
+    dept_time = TimeField(
+        label = "Departure Time",
+        validators=[DataRequired()]
+    )
+
+    arrival_time = TimeField(
+        label = "Arrival Time",
+        validators=[DataRequired()]
+    )
+
+    duration = IntegerField(
+        label = "Duration",
+        validators=[DataRequired()]
+    )
+
+    total_stops = IntegerField(
+        label = "Total Stops",
+        validators = [DataRequired()]
+    )
+
+    additional_info = SelectField(
+        label = "Additional Info",
+        choices = x_data.additional_info.unique().tolist(),
+        validators=[DataRequired()]
+    )
+    submit = SubmitField(
+        label = "Predict"
+    )
+
+
+
